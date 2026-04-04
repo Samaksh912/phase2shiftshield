4805b76a9974ccc3adfcd8355cbd6a37620467ea
Helixo613 <arnavbansal613@gmail.com>
2026-04-04 13:23:12 +0000

    Load backend env file explicitly

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({
+  path: path.resolve(__dirname, "..", ".env")
+});
 const { buildApp } = require("./app");
 const { getConfig } = require("./utils/config");
 
